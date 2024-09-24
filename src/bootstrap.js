474dda7cb9bfd5a07bbf293eae0b6b5c5c9dcf14
ToureBH <tbhcontratos@gmail.com>
2024-09-24 15:39:32 +0200

    connected recentposts to redux

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,29 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+const thunk = require('redux-thunk').thunk;
+import reducers from './reducers';
+
+
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+
+import './style/main.scss';
+
 import Home from './components/home';
 import Results from './components/results';
-import reducers from "./reducers";
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-import "./style/main.scss";
-
 
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
         <Switch>
-          <Route path="/" exact component={Home}/>
-          <Route path="/results" component={Results}/>
+            <Route path='/' exact component={Home}/>
+            <Route path='/results' component={Results}/>
         </Switch>
       </BrowserRouter>
-    </Provider>,
-    document.querySelector(".app-wrapper")
-  );
+    </Provider>
+    , document.querySelector('.app-wrapper'));
 }
 
-document.addEventListener("DOMContentLoaded", main);
+document.addEventListener('DOMContentLoaded', main);
